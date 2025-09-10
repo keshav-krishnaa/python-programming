aeae982e72d024bd8d675d6a006f7cbed2570ae2
K.B.KESHAV KRISHNAA <krishkrishnaa491@gmail.com>
2025-09-10 17:28:09 +0530

    Add files via upload

---
+++ accessing element.py
@@ -0,0 +1,7 @@
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a[0:1])
+print(a[-1:])
+print(a[0:3])
+print(a[-1::])
+print(a.insert[1,2])
+print(a[5])
---
+++ py.list.py
@@ -0,0 +1,6 @@
+a=[]
+n=int(input("Enter the no of elements:"))
+for i in range(0,n):
+ b=int(input("enter the number to be added:"))
+ a.append(b)
+print(a)
